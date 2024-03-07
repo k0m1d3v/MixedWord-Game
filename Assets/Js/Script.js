b9dbb52e62bc89c99fbc58388880b662973041bc
K0-M1 <86304359+k0m1d3v@users.noreply.github.com>
2024-03-07 10:43:39 +0100

    merging

--- Assets/Js/Script.js
+++ Assets/Js/Script.js
@@ -2,7 +2,7 @@ document.getElementById("Play-Btn").addEventListener("click", redirectPlay);
 document.getElementById("Leaderboard-Btn").addEventListener("click", redirectLeaderboard);
 
 function redirectPlay(){
-    window.location = "..\\Pages\\ComingSoon.html"; 
+    window.location = "..\\Pages\\Game.html"; 
 }
 
 function redirectLeaderboard(){
